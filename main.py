52239d5b4056b650bbe214c90841366f4c5a0cbe
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-01-07 21:23:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from tkinter import Tk
-from tkinter.ttk import Button
+from tkinter.ttk import Button, Label
 
 
 class Window(Tk):
     def __init__(self):
         super().__init__()
         self.wm_title("form")
-        self.geometry("320x200")
+        self.geometry("400x300")
 
         # create button, link it to clickExitButton()
-        exit_button = Button(self, text="Exit", command=self.clickExitButton)
-
+        exit_button = Button(self, text="Exit", command=self.recive)
         # place button at (0,0)
-        exit_button.place(x=50, y=60)
+        exit_button.place(x=50, y=100)
+        # show a label
+        self.label = Label(self,
+                      text='This is a label',
+                      font=('arial', 30, 'bold'))
+        self.label.place(x=50, y=10)
 
-    def clickExitButton(self):
-        print("exit")
-        self.destroy()
+    def recive(self):
+        self.label.config(text="hi")
+        # print("exit")
+        # self.destroy()
 
 
 if __name__ == "__main__":
